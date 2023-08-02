24983f27082af2c3e98d63fad1f6282201f6010b
PSIget <psi@s2ue.org>
2023-08-03 04:50:25 +0700

    Test

--- pages/api/og/index.tsx
+++ pages/api/og/index.tsx
@@ -1,5 +1,5 @@
-import { ImageResponse } from '@vercel/og';
-import { NextRequest } from 'next/server';
+// import { ImageResponse } from '@vercel/og';
+import { ImageResponse, NextRequest } from 'next/server';
 import { CSSProperties } from 'react';
 
 export const config = {
