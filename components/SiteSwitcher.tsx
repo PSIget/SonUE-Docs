6243354035cc282c54786aecb33a71958b862309
PSIget <psi@s2ue.org>
2023-11-09 17:07:53 +0700

    Fix color on Switcher

--- components/SiteSwitcher.tsx
+++ components/SiteSwitcher.tsx
@@ -68,11 +68,11 @@ function SiteSwitcher() {
       <span
         className={cn(
           "flex h-[34px] w-[134px] flex-shrink-0 items-center rounded-[8px] border border-[#dedfde] dark:border-[#333333] p-1 duration-300 ease-in-out",
-          "after:h-[24px] after:w-[76px] after:rounded-[4px] dark:after:bg-[#333333] after:shadow-sm after:duration-300 after:border dark:after:border-[#333333] after:border-[#666666]/100 after:bg-gradient-to-b after:from-[#3286F1] after:to-[#C33AC3] after:opacity-20 dark:after:opacity-100 dark:after:bg-none",
+          "after:h-[24px] after:w-[76px] after:rounded-[4px] dark:after:bg-[#333333] after:shadow-sm after:duration-300 after:border dark:after:border-[#333333] after:border-[#666666]/100 after:bg-[#333333] after:opacity-20 dark:after:opacity-100 dark:after:bg-none",
           "indeterminate:after:hidden",
           {
             "after:hidden": !site,
-            "after:w-[44px]": site === "game",
+            "after:w-[43px]": site === "game",
             "after:translate-x-[48px]": site === "editor",
           }
         )}
