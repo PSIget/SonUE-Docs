e472e99210e86e701f910e832beff1c97425a206
PSIget <psi@s2ue.org>
2023-11-12 21:29:06 +0700

    Slider Localized

--- components/HomePage/components/ScreenshotsSection/ScreenshotsSlider.tsx
+++ components/HomePage/components/ScreenshotsSection/ScreenshotsSlider.tsx
@@ -1,30 +1,70 @@
-import styles from "./ScreenshotsSlider.module.scss";
-import React, { useCallback, useState } from "react";
-
+import React, { useCallback, useState, FC } from "react";
 import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
-
 import Image from "next/image";
 import dynamic from "next/dynamic";
 
+import styles from "./ScreenshotsSlider.module.scss";
 import Data from "./data.json";
+import useLocalesMap, { ILocaleMap } from "utils/use-locales-map";
 
 interface ImageData {
-  name: string;
+  name: ILocaleMap<string>;
   url: string;
+}
+
+interface LightboxController {
+  toggler: boolean;
+  sourceIndex: number;
+}
+
+interface ImageSlideProps {
+  image: ImageData;
+  index: number;
+  openLightboxOnSlide: (index: number) => void;
 }
 
 const DynamicFsLightbox = dynamic(() => import("fslightbox-react"), {
   ssr: false,
 });
 
-export function ScreenshotsSlider() {
-  const [lightboxController, setLightboxController] = useState({
-    toggler: false,
-    sourceIndex: 0,
-  });
+const ImageSlide: FC<ImageSlideProps> = ({
+  image,
+  index,
+  openLightboxOnSlide,
+}) => {
+  const localizedName = useLocalesMap<string>(image.name);
 
-  const urls: string[] = Data.map((item: ImageData) => item.url);
+  return (
+    <SplideSlide key={`${image.name}-${index}`}>
+      <figure>
+        <Image
+          src={image.url}
+          alt={`Screenshot of the game - ${localizedName}`}
+          width={792}
+          height={444}
+          quality={100}
+          onClick={() => openLightboxOnSlide(index)}
+          sizes="(max-width: 768px) 294px, (max-width: 916px) 600px, 828px"
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+        />
+        <figcaption>{localizedName}</figcaption>
+      </figure>
+    </SplideSlide>
+  );
+};
+
+export const ScreenshotsSlider: FC = () => {
+  const [lightboxController, setLightboxController] =
+    useState<LightboxController>({
+      toggler: false,
+      sourceIndex: 0,
+    });
+
+  const urls = Data.map((item) => item.url);
 
   const openLightboxOnSlide = useCallback(
     (index: number) => {
@@ -67,43 +107,14 @@ export function ScreenshotsSlider() {
       >
         <SplideTrack>
           {Data.map((image, index) => (
-            <SplideSlide key={`${image.name}-${index}`}>
-              <figure>
-                <Image
-                  src={image.url}
-                  alt={`Screenshot of the game - ${image.name}`}
-                  width={792}
-                  height={444}
-                  quality={100}
-                  onClick={() => openLightboxOnSlide(index)}
-                  sizes="(max-width: 768px) 294px, (max-width: 916px) 600px, 828px"
-                  style={{
-                    width: "100%",
-                    height: "auto",
-                  }}
-                />
-                <Image
-                  src={image.url}
-                  alt={`Screenshot of the game - ${image.name}`}
-                  width={792}
-                  height={444}
-                  quality={100}
-                  className={styles.shadow}
-                  sizes="(max-width: 768px) 294px, (max-width: 916px) 600px, 828px"
-                  style={{
-                    width: "100%",
-                    height: "auto",
-                  }}
-                />
-                <figcaption>{image.name}</figcaption>
-              </figure>
-            </SplideSlide>
+            <ImageSlide
+              key={`${image.name}-${index}`}
+              image={image}
+              index={index}
+              openLightboxOnSlide={openLightboxOnSlide}
+            />
           ))}
         </SplideTrack>
-        <button className="splide__toggle hidden" type="button">
-          <span className="splide__toggle__play">Play</span>
-          <span className="splide__toggle__pause">Pause</span>
-        </button>
       </Splide>
       <DynamicFsLightbox
         toggler={lightboxController.toggler}
@@ -112,4 +123,4 @@ export function ScreenshotsSlider() {
       />
     </div>
   );
-}
+};
