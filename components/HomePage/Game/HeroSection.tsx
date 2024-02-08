49d46ff92bcabc3d97e5751ff35cd58191eba81e
PSIget <psi@s2ue.org>
2024-02-08 20:46:31 +0700

    Remove animations in `HeroSection`

--- components/HomePage/Game/HeroSection.tsx
+++ components/HomePage/Game/HeroSection.tsx
@@ -1,5 +1,4 @@
 import styles from "./HeroSection.module.scss";
-import { FadeIn } from "../components/FadeIn";
 import { HeroText, SectionSubtext } from "../components/Headings";
 import { btnInfo, btnDown, downText, firstTitle, firstTitleSub } from "./text";
 import { CTAButton } from "../components/CTAButton";
@@ -18,44 +17,33 @@ export default function HeroSection() {
 
   return (
     <>
-      <FadeIn
-        noVertical
+      <section
         className={`hero font-sans w-auto pb-16 pt-[48px] md:pb-24 lg:pb-32 md:pt-16 lg:pt-20 flex justify-between gap-8 items-center flex-col relative ${styles.hero}`}
-        section
       >
         <div className={`flex flex-col gap-6 ${styles.content}`}>
-          <FadeIn className="flex flex-col items-center justify-center gap-4 w-full">
+          <div className="flex flex-col items-center justify-center gap-4 w-full">
             <Logo height={320} className={styles.mainLogo} />
             <p className={`font-sans ${styles.sub}`}>WORK IN PROGRESS</p>
-          </FadeIn>
-          <FadeIn
-            delay={0.15}
-            className="flex flex-col items-center justify-center gap-5 px-6 text-center lg:gap-6"
-          >
+          </div>
+          <div className="flex flex-col items-center justify-center gap-5 px-6 text-center lg:gap-6">
             <HeroText>{firstTitleText}</HeroText>
             <SectionSubtext hero>{firstTitleSubText}</SectionSubtext>
-          </FadeIn>
+          </div>
         </div>
-        <FadeIn
-          delay={0.3}
-          className="flex flex-col items-center w-full gap-5 px-6"
-        >
+        <div className="flex flex-col items-center w-full gap-5 px-6">
           <div className="flex flex-col gap-3 w-full md:w-min md:!flex-row">
             <DownloadModal buttonText={buttonText} />
 
-            <Link
-              href="/game/docs"
-              className="block whitespace-nowrap"
-            >
+            <Link href="/game/docs" className="block whitespace-nowrap">
               <CTAButton outline limit>
                 {btnInfoText}
               </CTAButton>
             </Link>
           </div>
           <p className="text-sm text-[#666666]">{downloadText}</p>
-        </FadeIn>
+        </div>
         <AnimatedBackground />
-      </FadeIn>
+      </section>
     </>
   );
 }
