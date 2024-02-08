b382d2c7149f197cffd8f47ae3ffcddfc82fe363
PSIget <psi@s2ue.org>
2024-02-08 20:45:33 +0700

    Remove animations in `HeroSection`

--- components/HomePage/Editor/HeroSection.tsx
+++ components/HomePage/Editor/HeroSection.tsx
@@ -1,5 +1,4 @@
 import styles from "./HeroSection.module.scss";
-import { FadeIn } from "../components/FadeIn";
 import { HeroText, SectionSubtext } from "../components/Headings";
 import { btnInfo, btnDown, downText, firstTitle, firstTitleSub } from "./text";
 import { CTAButton } from "../components/CTAButton";
@@ -21,24 +20,16 @@ export default function HeroSection() {
 
   return (
     <>
-      <FadeIn
-        noVertical
+      <section
         className={`font-sans w-auto pt-16 md:pt-24 lg:pt-32 flex justify-between gap-8 items-center flex-col relative ${styles.hero}`}
-        section
       >
         <div className={`flex flex-col gap-6 ${styles.content}`}>
-          <FadeIn
-            delay={0.15}
-            className="flex flex-col items-center justify-center gap-5 px-6 text-center lg:gap-6"
-          >
+          <div className="flex flex-col items-center justify-center gap-5 px-6 text-center lg:gap-6">
             <HeroText>{firstTitleText}</HeroText>
             <SectionSubtext hero>{firstTitleSubText}</SectionSubtext>
-          </FadeIn>
+          </div>
         </div>
-        <FadeIn
-          delay={0.3}
-          className={`flex flex-col items-center w-full gap-5 px-6`}
-        >
+        <div className={`flex flex-col items-center w-full gap-5 px-6`}>
           <div className="flex flex-col gap-3 w-full md:w-min md:!flex-row">
             <DownloadModal buttonText={buttonText} />
 
@@ -49,10 +40,9 @@ export default function HeroSection() {
             </Link>
           </div>
           <p className="text-sm text-[#666666]">{downloadText}</p>
-        </FadeIn>
-        <FadeIn
+        </div>
+        <div
           className={`flex flex-col overflow-hidden items-center justify-center gap-4 w-full ![perspective:1000px] sm:![perspective:1000px] md:![perspective:1000px] lg:![perspective:1000px] ${styles.editorImage} `}
-          delay={0.5}
         >
           <Image
             src={ueScreenshot}
@@ -68,9 +58,9 @@ export default function HeroSection() {
               zIndex: -1,
             }}
           />
-        </FadeIn>
+        </div>
         <AnimatedBackground />
-      </FadeIn>
+      </section>
     </>
   );
 }
